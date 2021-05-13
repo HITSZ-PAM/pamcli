cd36a105855dbf4534673ee5331ef3678c64a07a
CH3CHOHCH3 <1978689494@qq.com>
2021-05-13 22:46:33 +0800

    test

--- cmd/run.go
+++ cmd/run.go
@@ -1,21 +1,124 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cmd
 
 import (
+	"os"
+	"fmt"
+	"os/exec"
+	"context"
+	"strings"
+	"syscall"
+	"os/signal"
+	"pamcli/modules/client"
 	"github.com/spf13/cobra"
 )
 
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run a program within PAM context",
+	Long: `Resolve environment variables and pass them to the target program`,
+	RunE: func(cmd *cobra.Command, args []string) error{
+
+		username,password,err := Get_account()
+		newEnv := os.Environ()
+		newEnv = append(newEnv,"PAM_ACCOUNT_USERNAME="+username)
+		newEnv = append(newEnv,"PAM_ACCOUNT_PASSWORD="+password)
+		
+		Son_Shell := exec.Command(args[0],args[1:]...)
+		
+		Son_Shell.Env = newEnv
+		Son_Shell.Stdin = os.Stdin
+		Son_Shell.Stdout = os.Stdout
+		Son_Shell.Stderr = os.Stderr
+
+		err = Son_Shell.Start()
+		if err != nil {
+			return err
+		}
+
+		done := make(chan bool, 1)
+		// Pass all signals to child process
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals)
+
+		go func() {
+			select {
+			case s := <-signals:
+				err := Son_Shell.Process.Signal(s)
+				if err != nil && !strings.Contains(err.Error(), "process already finished") {
+					fmt.Fprintln(os.Stderr, err.Error())
+				}
+			case <-done:
+				signal.Stop(signals)
+				return
+			}
+		}()
+		commandErr := Son_Shell.Wait()
+		done <- true
+
+		if commandErr != nil {
+			// Check if the program exited with an error
+			exitErr, ok := commandErr.(*exec.ExitError)
+			if ok {
+				waitStatus, ok := exitErr.Sys().(syscall.WaitStatus)
+				if ok {
+					// Return the status code returned by the process
+					os.Exit(waitStatus.ExitStatus())
+					return nil
+				}
+			}
+			return commandErr
+		}
+
+		return nil
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-var runCmd = &cobra.Command{
-	Use:     "run",
-	Short:   "Run a program within PAM context",
-	Long:    `Resolve environment variables and pass them to the target program`,
-	RunE:    runCmdExecute,
-	Example: "pamcli run -- <command>",
-}
-
-func runCmdExecute(cmd *cobra.Command, args []string) error {
-	return nil
+func Get_account()(string,string,error){
+	cfg := client.Config{
+		ServerAddr:   os.Getenv("PAM_SERVER_URL"),
+		ClientID:     os.Getenv("PAM_CLIENT_ID"),
+		ClientSecret: os.Getenv("PAM_CLIENT_SECRET"),
+	}
+	ctx := context.Background()
+	c, err := client.NewClient(ctx, &cfg)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create a new client:", err)
+		return "","",err
+	}
+	username,password,err := c.Resolve(os.Getenv("PAM_ACCOUNT_ID"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to resolve account:", err)
+		return "","",err
+	}
+	return username,password,nil
 }
--- go.mod
+++ go.mod
@@ -1,31 +1,11 @@
-module github.com/HITSZ-PAM/pamcli
+module pamcli
 
 go 1.15
 
 require (
-	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/go-oauth2/oauth2/v4 v4.2.0
+	github.com/HITSZ-PAM/pamcli v0.0.0-20210411035702-e1593090c298
 	github.com/go-resty/resty/v2 v2.6.0
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/gopherjs/gopherjs v0.0.0-20210202160940-bed99a852dfe // indirect
-	github.com/klauspost/compress v1.11.7 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/nxadm/tail v1.4.6 // indirect
-	github.com/onsi/ginkgo v1.15.0 // indirect
-	github.com/onsi/gomega v1.10.5 // indirect
-	github.com/smartystreets/assertions v1.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
-	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/tidwall/buntdb v1.1.8 // indirect
-	github.com/tidwall/gjson v1.6.8 // indirect
-	github.com/valyala/fasthttp v1.19.0 // indirect
-	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
-	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
-	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
-	google.golang.org/appengine v1.6.7 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/spf13/viper v1.7.1
 )
