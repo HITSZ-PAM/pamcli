d6d3795b0f3caa14226890a21c6c8b54a7ddec1a
Easton Man <manyang.me@outlook.com>
2021-10-30 16:41:31 +0800

    test: add bench mark

--- modules/client/client_test.go
+++ modules/client/client_test.go
@@ -41,3 +41,40 @@ func Test_client_Resolve(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_client_Resolve(b *testing.B) {
+	tests := []struct {
+		name string
+	}{
+		{name: "Valid"},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			cfg := client.Config{
+				ServerAddr:   os.Getenv("PAM_SERVER_URL"),
+				ClientID:     os.Getenv("PAM_CLIENT_ID"),
+				ClientSecret: os.Getenv("PAM_CLIENT_SECRET"),
+			}
+			ctx := context.Background()
+			c, err := client.NewClient(ctx, &cfg)
+			if err != nil {
+				b.Errorf("NewClient() error: %v", err)
+				return
+			}
+			got, got1, err := c.Resolve(os.Getenv("PAM_ACCOUNT_ID"))
+			for i := 0; i < b.N; i++ {
+				_, _, _ = c.Resolve(os.Getenv("PAM_ACCOUNT_ID"))
+			}
+			if err != nil {
+				b.Errorf("client.Resolve() error: %v", err)
+				return
+			}
+			if got != os.Getenv("PAM_ACCOUNT_USERNAME") {
+				b.Errorf("client.Resolve() got = %v, want %v", got, os.Getenv("PAM_ACCOUNT_USERNAME"))
+			}
+			if got1 != os.Getenv("PAM_ACCOUNT_PASSWORD") {
+				b.Errorf("client.Resolve() got1 = %v, want %v", got1, os.Getenv("PAM_ACCOUNT_PASSWORD"))
+			}
+		})
+	}
+}
